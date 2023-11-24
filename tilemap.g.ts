224d4feffc58b8484b3cab8022397fc85f132489
KTessa <151889546+KTessa@users.noreply.github.com>
2023-11-23 17:07:19 -0800

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Pothole":
+            case "tile1":return tile1;
+            case "Rocks":
+            case "tile2":return tile2;
+            case "Bush":
+            case "tile3":return tile3;
+            case "Trashcan":
+            case "tile4":return tile4;
+            case "Party hat":
+            case "tile6":return tile6;
+            case "Balloon0":
+            case "tile8":return tile8;
+            case "Balloon1":
+            case "tile9":return tile9;
+            case "Balloon3":
+            case "tile7":return tile7;
+            case "CakeLayer":
+            case "tile10":return tile10;
+            case "myTile":
+            case "tile12":return tile12;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
