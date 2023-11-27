04c6488392aaa54af5ce091e593ff0437ef5429f
AstoriaVia <149287233+AstoriaVia@users.noreply.github.com>
2023-11-27 00:27:05 -0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -7,19 +7,19 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile6 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile8 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile9 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile7 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile10 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile12 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -29,20 +29,20 @@ namespace myTiles {
             case "tile2":return tile2;
             case "Bush":
             case "tile3":return tile3;
+            case "CakeLayer":
+            case "tile10":return tile10;
+            case "myTile":
+            case "tile12":return tile12;
+            case "Balloon3":
+            case "tile7":return tile7;
+            case "Balloon1":
+            case "tile9":return tile9;
             case "Trashcan":
             case "tile4":return tile4;
             case "Party hat":
             case "tile6":return tile6;
             case "Balloon0":
             case "tile8":return tile8;
-            case "Balloon1":
-            case "tile9":return tile9;
-            case "Balloon3":
-            case "tile7":return tile7;
-            case "CakeLayer":
-            case "tile10":return tile10;
-            case "myTile":
-            case "tile12":return tile12;
         }
         return null;
     })
